7da8dc87e093a1fa1715376ff946d4abf4caac3a
wriga <49044905+wriga@users.noreply.github.com>
2019-11-20 18:45:10 -0500

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import './polyfills';
-
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+/** echarts extensions: */
+// import 'echarts-gl';
+// import 'echarts/theme/macarons.js';
+// import 'echarts/dist/extension/bmap.min.js'; 
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
